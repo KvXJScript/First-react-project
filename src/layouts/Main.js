18533ea9a586eb3ef3a1d0949bef20efc16ba40c
KvX <KvX989@gmail.com>
2020-07-13 08:53:37 +0200

    React router updated

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Home from "../pages/Home";
 import ToDoApp from "../pages/ToDoApp";
 import Api from "../pages/Api";
@@ -10,12 +10,14 @@ import Login from "../pages/Login";
 const Main = () => {
    return (
       <>
-      <Route path="First-react-project/" component = {Home} exact/>
-      <Route path="First-react-project/ToDoApp" component= {ToDoApp}/>
-      <Route path="First-react-project/AboutMe" component = {AboutMe}/>
-      <Route path="First-react-project/Api" component = {Api}/>
-      <Route path="First-react-project/Admin" component = {Admin}/>
-      <Route path="First-react-project/login" component = {Login}/>
+      <Switch>
+         <Route path="/First-react-project/" exact component = {Home} />
+         <Route path="/First-react-project/ToDoApp" component= {ToDoApp}/>
+         <Route path="/First-react-project/AboutMe" component = {AboutMe}/>
+         <Route path="/First-react-project/Api" component = {Api}/>
+         <Route path="/First-react-project/Admin" component = {Admin}/>
+         <Route path="/First-react-project/login" component = {Login}/>
+      </Switch>
       </>
    );
 }
--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -3,16 +3,16 @@ import "../styles/Nav.css";
 import {NavLink} from "react-router-dom";
 
 const list = [
-{name: "Home", path:"First-react-project/" ,exact:true},
-{name: "To Do App", path:"First-react-project/ToDoApp"},
-{name: "Api", path:"First-react-project/Api"},
-{name: "About Me", path:"First-react-project/AboutMe"},
-{name: "Admin", path:"First-react-project/Admin"},
+{name: "Home", path:"/First-react-project/", exact:true},
+{name: "To Do App", path:"/First-react-project/ToDoApp"},
+{name: "Api", path:"/First-react-project/Api"},
+{name: "About Me", path:"/First-react-project/AboutMe"},
+{name: "Admin", path:"/First-react-project/Admin"},
 ]
 const Nav = () => {
    const links = list.map(item =>(
       <li key={item.name}>
-      <NavLink to={item.path} exact = {item.exact? item.exact: false }>{item.name} </NavLink>
+      <NavLink to={item.path} exact = {item.exact ? item.exact: false }>{item.name} </NavLink>
       </li>
    ))
 
