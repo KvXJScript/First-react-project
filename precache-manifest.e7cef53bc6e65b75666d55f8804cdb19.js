e30b8f3d1a1abe813458d48d2c17f1ec4e4b0ccd
KvX <KvX989@gmail.com>
2020-07-13 08:44:55 +0200

    Updates

---
+++ precache-manifest.e7cef53bc6e65b75666d55f8804cdb19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "951b361a743339181414afbb742303f9",
+    "url": "/First-react-project/static/media/photo1.951b361a.png"
+  },
+  {
+    "revision": "30af5e5c6c92e80aa16a70195feb9c5e",
+    "url": "/First-react-project/static/media/buttonbackground.30af5e5c.png"
+  },
+  {
+    "revision": "3c34c857357980805d31",
+    "url": "/First-react-project/static/js/runtime~main.9da4faa5.js"
+  },
+  {
+    "revision": "31003028b405263d81e6",
+    "url": "/First-react-project/static/js/main.7675c344.chunk.js"
+  },
+  {
+    "revision": "c1c6317b1ffcdebf9434",
+    "url": "/First-react-project/static/js/2.2af20b41.chunk.js"
+  },
+  {
+    "revision": "31003028b405263d81e6",
+    "url": "/First-react-project/static/css/main.a3ebf5c5.chunk.css"
+  },
+  {
+    "revision": "248e9a15a12e7ab5f7ff4f140315a6cb",
+    "url": "/First-react-project/index.html"
+  }
+];
